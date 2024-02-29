7f6e3b284b319fc38931d1e8674a2d4d0b5d2e14
naimulhassan2001 <developernaimul00@gmail.com>
2024-02-29 10:44:36 +0600

    dynamic link done

--- lib/services/deep_linking_service.dart
+++ lib/services/deep_linking_service.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_deep_linking/core/app_route.dart';
+import 'package:get/get.dart';
 
 /// [DynamicLinkService]
 class DynamicLinkService {
@@ -10,16 +12,43 @@ class DynamicLinkService {
   static DynamicLinkService get instance => _singleton;
 
   // Create new dynamic link
-  Future<void> createDynamicLink() async {
+  createDynamicLink() async {
     final dynamicLinkParams = DynamicLinkParameters(
-      link: Uri.parse("https://flutterdeepdemo.page.link.com/?code=red"),
-      uriPrefix: "https://flutterdeepdemo.page.link",
-      androidParameters:
-          const AndroidParameters(packageName: "com.example.flutter_deep_linking"),
-    );
+        link: Uri.parse("https://flutterdeepdemo.page.link.com?screen=details"),
+        uriPrefix: "https://flutterdeepdemo.page.link",
+        androidParameters: const AndroidParameters(
+            packageName: "com.example.flutter_deep_linking"),
+        iosParameters: const IOSParameters(
+            bundleId: "com.example.flutter_deep_linking",
+            minimumVersion: "0",
+            appStoreId: '6473281791'));
     final dynamicLink =
         await FirebaseDynamicLinks.instance.buildShortLink(dynamicLinkParams);
 
     debugPrint("shortUrl ===========================> ${dynamicLink.shortUrl}");
+
+    return dynamicLink.shortUrl.toString();
+  }
+
+  Future<void> initDynamicLinks() async {
+    FirebaseDynamicLinks.instance.onLink.listen((dynamicLinkData) {
+      // Navigator.pushNamed(context, dynamicLinkData.link.path);
+
+      Map<String, String> param = dynamicLinkData.link.queryParameters;
+
+      String receivedCode = param['screen'] ?? " ";
+      String productId = param['productId'] ?? " ";
+
+      if (receivedCode == "details") {
+        Get.toNamed(AppRoute.home);
+      }
+
+      print(
+          "=========================================>receivedCode ${receivedCode}");
+      print("=========================================>productId ${productId}");
+    }).onError((error) {
+      print('onLink error');
+      print(error.message);
+    });
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import app_links
+import firebase_analytics
 import firebase_core
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
+  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
 }
